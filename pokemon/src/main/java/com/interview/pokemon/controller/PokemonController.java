ebaac5471df236c17e8bbcab73b022df393839ab
Louise Skinner <louiseskinner@live.co.uk>
2025-02-26 17:15:09 +0000

    allow for connection

--- pokemon/src/main/java/com/interview/pokemon/controller/PokemonController.java
+++ pokemon/src/main/java/com/interview/pokemon/controller/PokemonController.java
@@ -3,6 +3,7 @@ package com.interview.pokemon.controller;
 import com.interview.pokemon.model.Pokemon;
 import com.interview.pokemon.service.PokemonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("pokemon")
+@CrossOrigin
 public class PokemonController {
 
     @Autowired
