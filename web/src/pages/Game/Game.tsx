17eb65f4a120cb57302e8ac246737360f8cadc1b
Louise Skinner <louiseskinner@live.co.uk>
2025-02-28 12:26:08 +0000

    get svg

--- web/src/pages/Game/Game.tsx
+++ web/src/pages/Game/Game.tsx
@@ -1,9 +1,14 @@
-import { Card, Spinner } from "react-bootstrap";
+import { Button, Card, Spinner } from "react-bootstrap";
 import useGetPokemon from "../../hooks/useGetPokemon";
 import cx from 'classnames'
 import React from "react";
 import './Game.scss'
-import { getRandomNumber } from "../../utils/getRandomNumber.utils";
+import { capitalizeFirstLetter, getRandomNumber } from "../../utils/utils";
+
+
+// Fix Spinner
+// Add colours for pass and fail
+// Loop for 10, display result
 
 const Game = () => {
     const { data, isLoading, error } = useGetPokemon();
@@ -11,7 +16,8 @@ const Game = () => {
     const [selectedNumbers, setSelectedNumbers] = React.useState<number[]>([])
     const [randomPokemonIndex, setRandomPokemonIndex] = React.useState(0);
     const [score, setScore] = React.useState(0);
-    const displaySilhouette = !hasSelectedResponse ? cx("card", "img", "silhouette") : cx("card", "img");
+    const [svgContent, setSvgContent] = React.useState<string>();
+    const displaySilhouette = !hasSelectedResponse ? cx("img", "silhouette") : cx("img");
 
     React.useEffect(() => {
         let newSelectedNumbers = [...selectedNumbers]; // Start with the current array
@@ -24,41 +30,81 @@ const Game = () => {
         setSelectedNumbers(newSelectedNumbers); // Update state after array is built
         const randomIndex = newSelectedNumbers[Math.floor(Math.random()* newSelectedNumbers.length)];
         setRandomPokemonIndex(randomIndex);
-
     }, [hasSelectedResponse]);
 
-    if (isLoading) return <div><Spinner animation="border" role="status">Loading...</Spinner></div>;
-    if (error) return <div>Error: {error}</div>;
+    const loadSvg = async (url: string): Promise<string | undefined> => {
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return await response.text(); // Returns the fetched SVG content as a string
+        } catch (error) {
+          console.error("Error loading SVG:", error);
+          return url; // Return undefined in case of failure
+        }
+      };
+
+    React.useEffect(() => {
+        const fetchAndSetSvg = async () => {
+            if(data) {
+                const svgData = await loadSvg(data[randomPokemonIndex].image);
+                if (svgData) {
+                    const encodedSvg = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svgData.replace(/^<\?xml.*\?>\s*/, ""))}`;
+                    setSvgContent(encodedSvg);
+                }
+            }
+          };
+      
+          fetchAndSetSvg();
+    }, [randomPokemonIndex]);
 
     const onClickHandler = (selection: string) => {
         setSelectedNumbers([]);
-        setHasSelectedResponse(!hasSelectedResponse);
         if(data && data[randomPokemonIndex].name == selection) {
             setScore(score + 1);
-
         }
+        setHasSelectedResponse(!hasSelectedResponse);
     }
 
     const displayOptions = () => (
         <div className="question">
-            {selectedNumbers.map(value =>
-                <Card.Title className="option" onClick={() => onClickHandler(data[value].name)}>{data && data[value].name}</Card.Title>
+            {data && selectedNumbers.map(value =>
+                <Card.Title
+                    key={data[value].id}
+                    className="option"
+                    onClick={() => onClickHandler(data[value].name)}
+                >
+                {capitalizeFirstLetter(data[value].name)}
+                </Card.Title>
             )}
         </div>
     )
 
+    if (isLoading) return <div><Spinner animation="border" role="status">Loading...</Spinner></div>;
+    if (error) return <div>Error: {error}</div>;
+
+    console.log
+
     return (
         <div className="container">
-            <div><h4>Score: {score}/10</h4></div>
-            <Card>
-                {data && <Card.Img className={displaySilhouette} src={data[randomPokemonIndex].image} />}
-                <Card.Title className="title">Who's That Pokemon?</Card.Title>
-                <Card.Body>
-                    <div className="questions">
-                        {displayOptions()}
-                    </div>
-                </Card.Body>
-            </Card>
+            {data &&
+            <>
+                <div>
+                    <h4>Score: {score}/10</h4>
+                </div>
+                <Card className="game">
+                    <Card.Title className="title">Who's That Pokemon?</Card.Title>
+                    <img className={'z-0 w-full'} src={svgContent}/>
+                    <Card.Body>
+                        <div className="questions">
+                            {displayOptions()}
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Button disabled={!hasSelectedResponse}>Submit</Button>
+            </>
+           }
         </div>
     )
 }
