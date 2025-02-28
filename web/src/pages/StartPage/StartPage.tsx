7f8cb050bb61367dd215e87d8958e5a9019a6580
Louise Skinner <louiseskinner@live.co.uk>
2025-02-28 09:41:58 +0000

    Add img

--- web/src/pages/StartPage/StartPage.tsx
+++ web/src/pages/StartPage/StartPage.tsx
@@ -1,6 +1,7 @@
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import './StartPage.css'
+import img from '../../assets/whos_that.png'
 
 const StartPage = () => {
     const history = useHistory();
@@ -9,6 +10,9 @@ const StartPage = () => {
     }
     return (
         <div>
+            <Card>
+                <Card.Img src={img}/>
+            </Card>
             <Button className='start_button' onClick={onClickHandler}>Game Start</Button>
         </div>
     )
