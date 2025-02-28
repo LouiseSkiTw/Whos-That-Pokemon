8a23f7354ff75cd6604fd79f1c6f98de490b7a3c
Louise Skinner <louiseskinner@live.co.uk>
2025-02-28 09:40:58 +0000

    update routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -5,7 +5,7 @@ import Game from "./pages/Game/Game";
 const Routes = () => (
     <Switch>
         <Route exact path='/' render={()=> <StartPage />} />
-        <Route path='/game' render={() => <Game />} />
+        <Route path='/game' component={() => <Game />} />
     </Switch>
 )
 
