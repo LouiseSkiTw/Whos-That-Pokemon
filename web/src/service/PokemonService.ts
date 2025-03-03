72ba4166d58ada07a3acb53dde1f7d7098d0cf65
Louise Skinner <louiseskinner@live.co.uk>
2025-03-03 14:28:22 +0000

    add timeout

--- web/src/service/PokemonService.ts
+++ web/src/service/PokemonService.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 export const BASE_URL = 'http://localhost:8080/pokemon'
 const getPokemon = async() => {
-    const value = await axios.get(BASE_URL+'/getPokemon').then(response => response.data);
-    return value;
+        const value = await axios.get(BASE_URL+'/getPokemon',  {
+            timeout: 5000,
+          }).then(response => response.data);
+        return value;
 }
 
 export {getPokemon};
